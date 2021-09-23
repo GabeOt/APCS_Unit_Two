fad8d1f00960baea9bffc5c22d00e6265a2faad5
GabeOt <gabriel.otridge@ssfs.org>
2021-09-23 14:57:11 -0400

    Completed the exercise

--- src/PointsAndRectangles.java
+++ src/PointsAndRectangles.java
@@ -6,8 +6,25 @@ import java.util.Scanner;
 public class PointsAndRectangles
 {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the X coordinate of the top left corner of the rectangle?");
+        int xCoord = scan.nextInt();
+        System.out.println("What is the Y coordinate of the top left corner of the rectangle?");
+        int yCoord = scan.nextInt();
+        Point objectp = new Point(xCoord, yCoord);
+        System.out.println("What is the length of the rectangle?");
+        int length = scan.nextInt();
+        System.out.println("What is the width of the rectangle?");
+        int width = scan.nextInt();
+        Dimension objectd = new Dimension(width, length);
+        Rectangle objectr = new Rectangle(xCoord, yCoord, width, length);
+        System.out.println(objectp);
+        System.out.println(objectd);
+        System.out.println(objectr);
+
+
+
         Scanner kb = new Scanner(System.in);
         // create print statements and get input for the x and y values.
 

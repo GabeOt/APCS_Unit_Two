b6b38bfa97106fdc0558d27a92083b582002bbb1
GabeOt <gabriel.otridge@ssfs.org>
2021-09-27 08:51:28 -0400

    Completed the exercise

--- src/FightSong.java
+++ src/FightSong.java
@@ -6,13 +6,36 @@ public class FightSong {
     only be used in this class. Since I need to access it for testing purposes, it must be public.
      */
     public static void goTeam() {
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
+    }
+
+    public static void yTB() {
+        System.out.println("You're the best,");
+        System.out.println("In the West.");
+    }
+    public static void space() {
+        System.out.println("");
+    }
+
         // Add any print statements you think should go in this method.
 
-    }
+
 
     // Create any additional methods you need in this space in order to fulfill the requirements of the program
 
     public static void main(String[] args) {
+        goTeam();
+        space();
+        goTeam();
+        yTB();
+        goTeam();
+        space();
+        goTeam();
+        yTB();
+        goTeam();
+        space();
+        goTeam();
         // No print statements should be in here, only method calls.
 
     }

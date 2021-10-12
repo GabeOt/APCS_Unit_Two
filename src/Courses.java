b7b8a06b7d1b68ffaa9919f70a8ab49ca72e780f
GabeOt <gabriel.otridge@ssfs.org>
2021-10-12 10:37:31 -0400

    Completed the exercise

--- src/Courses.java
+++ src/Courses.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Courses {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the description of the course?");
+        String description = scan.nextLine();
+        int length = description.length();
+        int courseNameNum = description.indexOf(" ");
+        String courseName = description.substring(0, courseNameNum);
+        System.out.println("Department: " + courseName);
+        int courseNumNum = description.indexOf(" " , courseNameNum+1);
+        String courseNum = description.substring(courseNameNum, courseNumNum);
+        System.out.println("Course Number:" + courseNum);
+        String courseTitle = description.substring(courseNumNum, length);
+        System.out.println("Title:" + courseTitle);
+
+
+
+
+
+
+
+
+    }
 }
